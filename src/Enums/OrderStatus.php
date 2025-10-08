407aac19eb5ba317aef426b5f3a976b5b4cbf499
DevMahbubHussain <devmahbubmamun@gmail.com>
2025-10-08 11:22:35 +0600

    Composer initiated

--- src/Enums/OrderStatus.php
+++
@@ -1,9 +0,0 @@
-<?php 
-namespace App\Enums;
-enum OrderStatus: string {
-    case CREATED = 'created';
-    case CONFIRMED = 'confirmed';
-    case SHIPPED = 'shipped';
-    case DELIVERED = 'delivered';
-    case CANCELLED = 'cancelled';
-}
